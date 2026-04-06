db65f7332a26679f13dfe5f693182fe9842db0b9
yarivha <yariv.hakim@gmail.com>
2026-04-06 13:36:13 +0000

    add route for reports delete

--- scmserver/src/main.rs
+++ scmserver/src/main.rs
@@ -145,6 +145,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/policies/download/{id}",get(policies_report_download))
         .route("/reports", get(reports))
         .route("/reports/save/{id}",get(reports_save))
+        .route("/reports/delete/{id}",get(reports_delete))
         .route("/send", post(send))
         .route("/result", post(receive_result))
         .route("/{*path}", get(|axum::extract::Path(path): axum::extract::Path<String>| async move {
